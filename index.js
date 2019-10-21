b750ff4eb37d884e45bc93865ad00822cabcc23a
Atheeralaghbari <atheeralaghbari61@gmail.com>
2019-10-21 22:47:56 +0330

    update

--- index.js
+++ index.js
@@ -3,19 +3,23 @@ function validateName(){
     if (name.length==0){
         // name.style.border="1px solid red";
         producePrompt("Name is required","commentNamePromt","red");
-       
+        document.getElementById("Commentname").style.border="1px solid red"
+
         return false;
    
     }
     if (name.match(/[0-9]/))
     {
         producePrompt("name must be letters","commentNamePromt","red");
+        document.getElementById("Commentname").style.border="1px solid red"
+
         return false;
     }
     if(!name.match(/^[A-Za-z]*\s{1}[A-Za-z]*$/))
     {
       
         producePrompt("Firstname Lastname please ","commentNamePromt","red");
+        document.getElementById("Commentname").style.border="1px solid red"
         return false;
     }
    
@@ -30,11 +34,14 @@ function validatePhone(){
     var phone =document.getElementById("Commentphone").value;
     if (phone.length==0){
         producePrompt("phone number is required","commentphonePromt","red");
+        document.getElementById("Commentphone").style.border="1px solid red"
         return false;
     }
     if(!phone.match(/^(77|73|71|70)([0-9]{7})$/))
     {
         producePrompt("phone number is invalid","commentphonePromt","red");
+        document.getElementById("Commentphone").style.border="1px solid red"
+
         return false;
     }
 else{
@@ -48,15 +55,20 @@ function validateEmail(){
     var email=document.getElementById("Commentemail").value;
     if (email.length==0){
         producePrompt("email is required","commentemailPromt","red");
+        document.getElementById("Commentemail").style.border="1px solid red"
+
         return false;
     }
     if(!email.match(/^[A-Za-z\.-0-9]*[@][A-Za-z]*[\.][a-z]{2,4}$/))
     {
     producePrompt("invaild  Email ","commentemailPromt","red");
+    document.getElementById("Commentemail").style.border="1px solid red"
+
     return false;
         }
     producePrompt("vaild  Email ","commentemailPromt","green");
-    return teue;
+    
+    return true;
 }
 
 
@@ -110,12 +122,13 @@ function Age_validator(){
     if (isNaN(age) || age < 1 || age > 100)
     { 
         producePrompt("unvalid age","dob1","red");
+        document.getElementById("age").style.border="1px solid red"
+
       return false;
     }
 }
 function producePrompt(message,promptLocation,color){
     document.getElementById(promptLocation).innerHTML=message;
     document.getElementById(promptLocation).style.color=color;
-
 
 }
